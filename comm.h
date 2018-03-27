c3aec33aaa3690cea3fb2f9711c4f933a618bee6
Jarod White <jrwhite20@gmail.com>
2018-03-27 08:06:48 -0500

    Updated protocol documentation

--- comm.h
+++ comm.h
@@ -27,9 +27,11 @@
 #define DATA_SUBHEAD_TYPE           0b001
 #define DATA_SUBHEAD_STATE          0b010
 
+// Message templates for making NID Pings and Data-to-NID messages
 #define NID_PING_MESSAGE            0b11100000000000000000000000000000
-#define DATA_MESSAGE                0b10100000000000000000000000000000 // (NID) (KEEP ALIVE=0) (sub-header) (CHANNEL= NONE) (DATA) (no data)
+#define DATA_MESSAGE                0b10100000000000000000000000000000 
 
+// Global commands
 #define IDENTIFY_COMMAND            0b000001
 #define VERSION_COMMAND             0b000010
 #define PAUSE_COMMAND               0b000011
@@ -49,49 +51,165 @@
 #define NEXT_BIT(m)   (1 << (m.length-1)) & m.message
 
 /*
-    This and comm.c define all communication protocol
-    message headers:
+    This and comm.c define all communication protocol using the NeuroBytes protocol specifiction
+
+    All packets are between 4 and 32 bits and length and begin with 1-bit high
+    and a 3-bit message header.
+    [1-bit high] [3-bit header]
+
+    The message headers are:
     0b000  -   Unused
     0b001  -   Blink (Debug)
-    0b010  -   Data to NID (6-bit data header + 16-bit data packet + 3-bit parity check)
+    0b010  -   Data to NID
     0b011  -   Downstream Ping
     0b100  -   NID Global Command 
     0b101  -   NID Selected Command 
     0b110  -   NID Ping 
     0b111  -   Downstream Pulse
-    There are three types of communication that are defined in this protocol:
+
+    There are three types of communication that are supported in this protocol:
     
     1. Network Interface Device (NID) broadcasting to all neurons in the network. 
-    
-        Messages include nid pings and identify device request. The NID commands class of messsages
-        
-        List of message headers:
-        -NID_PING_HEADER
-            +read 6-bit distance counter
-                -processNIDPING
-        -NID_GLOBAL_HEADER
-            +read 6-bit command header
-                -IDENTIFY_COMMAND
-                    +read 3-bit channel
-                        -processIdentifyCommand
-                -VERSION_COMMAND
-                    +read 13-bit packet (5-bit device id + 8-bit version)
-                        -processVersionCommand
-                -RESUME_COMMAND
-                    -set run_flag
-                -PAUSE_COMMAND
-                    -clear run_flag
-        -NID_SELECTED_HEADER
-            +read 24-bit packet (3-bit channel + 5-bit parameter + 16-bit data)
-                -SET_PARAMETER_COMMAND
-                    -processParameterCommand
-        -BLINK_HEADER
-            -set blink_flag
+
+        Messages are sent by NID and received by all devices on the network.
+        Global Sommands are processed by every device on the network.
+        Selected Commands are processed only by the device with the channel specified by the message.
+
+        Included message headers:
+        0b001 - Blink
+        0b100 - NID Global Command - Send a command to the entire network
+        0b101 - NID Selected Command - Send a command to a previously selected device
+        0b110 - NID Ping - Propogate ping to update NID route
+
+        ####### NID Global Command ########
+
+        NID Global Command structure:
+        [1-bit high]
+        [3-bit header]
+        [6-bit command]             - See list of global commands below
+        [More depending on command]
+
+        Global Commands:
+        0b001 - Identify. Followed with 3-bit channel
+        0b010 - Version. Followed by 16-bit board id and version number. Used to blink out-of-date boards.
+        0b011 - Pause.
+        0b100 - Play.
+        0b101 - Zero
+        0b111 - Span
+
+            *Note: 6-bits is probably excessive for global commands. Might change in the future.
+
+        Example NID Global Command:
+
+        1           [1-bit high]
+        100         [NID Global Command Header]
+        000001      [Identify Command]
+        010         [Channel 2]
+
+        ######## NID Selected Command ########
+
+        NID Selected Command:
+        Used to either send a command to a specific neuron (e.g. go into learning mode)
+        or change the value of an operating parameter (e.g. set dendrite 1 to 120%)
+
+        NID Selected Command Structure:
+        [1-bit high]
+        [3-bit header]      - always NID Selected Command Header (0b101)
+        [3-bit channel]
+        [1-bit change parameter flag]
+        [4-bit command] OR [4-bit parameter ID]     - depending on if [change paramater flag] is set
+        (optional) [16-bit data]
+
+        NOTE: Parameter IDs are board-specific (i.e. different for every board). 
+        They are not all fully specified yet. Currently Interneurons and Motor Neurons are set.
+        Their values can be found in their repos (main.c). 
+
+        Example NID Selected Command:
+
+        Set the value of dendrite 1 on an interneuron on channel 4 to 256
+        1                   [1-bit high]
+        101                 [NID Selected Command Header]
+        100                 [Channel 4]
+        1                   [Change Parameter]
+        0010                [Dendrite 1]
+        0000001000000000    [Change to 256]
+        ________________
+        = 0b1101100100100000001000000000
+
+        ######## NID Ping ########
+
+        Functional Summary:
+
+        NID pings are periodically sent (~200 ms) by NID to the whole network in order to 
+        update the shortest-route-to-NID memorized by every device.
+
+        The NID ping packet contains a 6-bit distance field which is incremented by every successive neuron.
+        The shortest route is determined by every neuronto be through the dendrite/axon that receives
+        the NID ping with the shortest distance value. Memorized NID connections are lost if a NID ping
+        is not received within NID_PING_TIME (~1000 ms).
+
+        NID Ping Message Structure:
+        [1-bit high]
+        [3-bit header]      - always NID Ping Header (0b110)
+        [6-bit distance]
+
+        NID Ping Example Message:
+
+        NID Ping relayed by two neurons so far
+        1           [1-bit high]
+        110         [NID Ping Header]
+        000010      [distance = 2]
+        _______
+        = 0b111000010
+
+
             
     2. Selected neurons sending data to the NID.
 
+        Messages are sent by identified devices and are relayed along the shortest path to the NID.
+        The shortest path to the NID is maintained by NID pings.
+
+        Messages sent from identified devices to the NID. Capable of sending many different types
+        of data (16-bits) to the NID.
+
+        Included message header:
+        0b010 - Data to NID
+
+        ######## Data to NID #######
+
+        Data message structure:
+        [1-bit high]            
+        [3-bit header]              - 0b010 (Data Header)
+        [3-bit subheader]           - specifies type of data being transmitted
+        [3-bit channel]             - specifies channel sending the data
+        [1-bit fire flag]           - flag indicating the neuron fired
+        [4-bit parameter id]        - (optional) further specifies the type of data (e.g. dendrite weighting number)
+        [16-bit data]
+
+        Subheaders:
+        0b000   - membrane potential
+        0b001   - board type
+        0b010   - unique id
+        0b011   - operating mode
+        0b100   - board-specific parameter
+
+        At this time, board-specific parameters (for Parameter ID) have not been fully assigned.
+        But some values can be found in their respective firmware repos in main.c
+
+        Example:
+        Data message from an interneuron on channel 3 sending its membrane potential:
+        0b[1][010][000][011][0][0000][0000001101011100]
+
+
     3. Upstream neurons sending pulses to downstream neurons (axon -> dendrite).
 
+        All neuron->neuron communications are maintained by the downstream ping which distinguishes
+        excitatory and inhibitory connections.
+
+        Only two messages sent in this category
+
+        0b1011 - Downstream ping message
+        0b1111 - Downstream pulse message
 */
 
 
